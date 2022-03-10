ee2c3037882a16c59db133f4b0968727e3386444
Goldar Luu <gluu41@gmail.com>
2022-03-09 19:04:24 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,9 +5,8 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test 
     public void addition() {
-        assertEquals(5, SkillDemo.add(2,4)); 
+        assertEquals(5, SkillDemo.add(2,3)); 
     }
 }
  // We have to get the test files first lol
 
- 
