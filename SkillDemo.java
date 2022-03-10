7bb20989b21a25dfef3f8159329b28cf5c925b3f
Goldar Luu <gluu41@gmail.com>
2022-03-09 19:05:23 -0800

    added multiplication

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,6 +1,6 @@
 public class SkillDemo {
-    public static int add(int num1, int num2) {
-        return num1 + num2; 
+    public static int multiply(int num1, int num2) {
+        return num1 * num2; 
     }
 
 } 
--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,8 +4,8 @@ import org.junit.*;
 
 public class SkillDemoTest {
     @Test 
-    public void addition() {
-        assertEquals(5, SkillDemo.add(2,3)); 
+    public void multiplication() {
+        assertEquals(5, SkillDemo.multiply(2,3)); 
     }
 }
  // We have to get the test files first lol
